60d17a68769efae2a7034712d355bd22aa4a3e41
Ricardo Garcia Forte <ricardogarciaforte@gmail.com>
2022-06-28 12:36:28 -0500

    Project hr-worder created.

---
+++ hr-worker/src/test/java/mx/com/udemy/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.udemy.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
