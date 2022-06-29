b0f2fe7a96e74544012ba36a3a8b80799b7031fd
Ricardo Garcia Forte <ricardogarciaforte@gmail.com>
2022-06-29 16:23:53 -0500

    Rest Template

--- hr-payroll/src/main/java/mx/com/udemy/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/mx/com/udemy/hrpayroll/service/PaymentService.java
@@ -1,13 +1,31 @@
 package mx.com.udemy.hrpayroll.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import mx.com.udemy.hrpayroll.entity.Payment;
+import mx.com.udemy.hrpayroll.entity.Worker;
 
 @Service
 public class PaymentService {
+	
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Rick", 200.0, days);
+		
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", ""+workerId);
+		Worker worker = this.restTemplate.getForObject(this.workerHost + "/workers/{id}", Worker.class, uriVariables);
+		
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
