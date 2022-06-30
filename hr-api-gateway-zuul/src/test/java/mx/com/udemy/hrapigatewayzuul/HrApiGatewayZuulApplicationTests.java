6efbabbd9db9df0a7d3b490fb0f25c72f13166b4
Ricardo Garcia Forte <ricardogarciaforte@gmail.com>
2022-06-30 15:07:12 -0500

    Project hr-api-gateway-zuul created

---
+++ hr-api-gateway-zuul/src/test/java/mx/com/udemy/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.udemy.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
