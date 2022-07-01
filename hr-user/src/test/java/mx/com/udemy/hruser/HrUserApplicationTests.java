8d6d48da805c7a09c551d3129c6c3e8f345bb4b8
ricardogarciaforte <ricardogarciaforte@gmail.com>
2022-07-01 12:47:03 -0500

    Project hr-user creates

---
+++ hr-user/src/test/java/mx/com/udemy/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.udemy.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
